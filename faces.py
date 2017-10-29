c8ad42829e482c692d9bc2b4612db31e9cf1ae41
Igor Ramazanov <themirrortruth@gmail.com>
2017-10-29 15:25:00 +0100

    Using stopping criteria

--- faces.py
+++ faces.py
@@ -1,14 +1,21 @@
 #!/usr/local/bin/python3.6
 
 """
-This script prints precision measurements of an ANN with various learning rates and iteration counts.
-"""
+Trains an ANN on training Images and prints classification results for testing Images.
 
+Using:
+python main.py \
+<path to file with training images> \
+<path to file with answers for training images> \
+<path to file with testing images>
+"""
 from functools import reduce
+import sys
 import numpy as np
 
-LEARNING_RATES = [0.0001, 0.001, 0.01, 0.1, 1, 5]
-ITERATIONS_COUNTS = [10, 100, 1000]
+LEARNING_RATE = 0.005
+PRECISION_THRESHOLD = 0.68
+TRAINING_SET_PERCENTAGE = 0.7
 
 
 class Image:
@@ -16,6 +23,7 @@ class Image:
         self.id = id
         self.pixels = np.array([])
         self.answer = -1
+
 
 def read_images(file_name):
     """
@@ -95,7 +103,7 @@ def read_answers(images, file_name):
         image.answer = convert_answer_to_array(int(answer))
 
 
-def train(training_set, learning_rate, iterations_count):
+def train(training_set, testing_set, learning_rate):
     """
     Trains on training set of images and returns `prediction_function` for image classification.
 
@@ -107,25 +115,27 @@ def train(training_set, learning_rate, iterations_count):
     Returns:
         function: prediction function that receives an Image and returns emotion code.
     """
-    np.random.seed(1)
     weights_matrix = np.random.random((400, 4))
 
     def calc_current_output(image):
         weight_sums = np.dot(image.pixels, weights_matrix)
         return 1.0 / (1.0 + np.exp(-weight_sums))
 
-    for i in range(iterations_count):
-        for image in training_set:
-            current_output = calc_current_output(image)
-            error_layer = image.answer - current_output
-            weights_deltas_matrix = np.reshape(image.pixels, (400, 1)) * error_layer * learning_rate
-            weights_matrix += weights_deltas_matrix
-
     def prediction_function(image):
         output = calc_current_output(image)
         return np.argmax(output) + 1
 
-    return prediction_function
+    def is_good_enough():
+        return measure_precision(prediction_function, testing_set) > PRECISION_THRESHOLD
+
+    while True:
+        for image in training_set:
+            if is_good_enough():
+                return prediction_function
+            current_output = calc_current_output(image)
+            error_layer = image.answer - current_output
+            weights_deltas_matrix = np.reshape(image.pixels, (400, 1)) * error_layer * learning_rate
+            weights_matrix += weights_deltas_matrix
 
 
 def measure_precision(f, testing_set):
@@ -140,6 +150,7 @@ def measure_precision(f, testing_set):
     Returns:
         float: Percent of properly classified Images, changes from [0.0, 1.00]
     """
+
     def is_right(image):
         predicted_answer = f(image)
         return int(predicted_answer == convert_array_to_answer(image.answer))
@@ -148,28 +159,20 @@ def measure_precision(f, testing_set):
     return count_of_right / len(testing_set)
 
 
+def print_answers(images, prediction_function):
+    for image in images:
+        print("%s %d" % (image.id, prediction_function(image)))
+
+
 if __name__ == '__main__':
-    training_file_path = "training.txt"
-    images = read_images(training_file_path)
-    read_answers(images, "training-answers.txt")
+    training_images = read_images(sys.argv[1])
+    read_answers(training_images, sys.argv[2])
+    testing_images = read_images(sys.argv[3])
 
-    cross_validation_sets = [
-        (images[100:], images[0:100]),
-        (images[0:100] + images[200:], images[100:200]),
-        (images[:200], images[200:])
-    ]
+    images_count = len(training_images)
+    training_set_size = int(images_count * TRAINING_SET_PERCENTAGE)
+    training_set = training_images[0:training_set_size]
+    testing_set = training_images[training_set_size:]
 
-    precisions = []
-
-    for rate in LEARNING_RATES:
-        print("Learning rate: ", rate)
-        precisions.append([])
-        for i_count in ITERATIONS_COUNTS:
-            print("     Iteration count: ", i_count)
-            ps = []
-            for training_set, testing_set in cross_validation_sets:
-                prediction_function = train(training_set, rate, i_count)
-                precision = measure_precision(prediction_function, testing_set)
-                ps.append(precision)
-            precisions[-1].append(np.average(np.array(ps)))
-            print("     Precision: %4.2f\n" % precisions[-1][-1])
+    prediction_function = train(training_set, testing_set, LEARNING_RATE)
+    print_answers(testing_images, prediction_function)
